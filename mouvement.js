aa2d141f48ed699580707148185b0becea6d6892
EvaGms <eva.g@codeur.online>
2017-10-23 15:41:06 +0200

    montagnes

---
+++ mouvement.js
@@ -0,0 +1,19 @@
+window.onkeydown = function(e) {
+    var key = e.keyCode || e.which;
+    switch (key) {
+        case 37:
+        
+        break;
+        case 39:
+        
+        break;
+        case 38:
+        
+        break;
+        case 40:
+        
+        break;
+    default:
+        break;
+    }
+};
